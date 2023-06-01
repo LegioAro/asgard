3948da32276052d7e95874b21063adc2c6941d6b
LegionAro <u@example.com>
2023-06-01 08:18:00 +0300

    scroll smooth

--- js/main.js
+++ js/main.js
@@ -233,3 +233,21 @@ function isModalClose() {
   }
 }
 isModalClose();
+
+//smooth
+
+const scrollSmoothLinck = document.querySelectorAll('*[data-scroll-smooth]');
+
+for (let elem of scrollSmoothLinck) {
+  elem.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    let blockID = elem.getAttribute('data-scroll-smooth');
+    let top = document.getElementById(blockID).getBoundingClientRect().top;
+
+    document.querySelector('html,body').scrollTo({
+      top: top + window.pageYOffset - 130,
+      behavior: 'smooth',
+    });
+  });
+}
